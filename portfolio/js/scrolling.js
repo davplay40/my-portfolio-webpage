73042693371e17b75af01e89cc5edc9553ab0009
Daveplay40 <fishfamily1@live.com>
2025-03-12 17:52:05 +0000

    Update scrolling.js
    ~ able to change the grid template columns deepens on screen width

--- portfolio/js/scrolling.js
+++ portfolio/js/scrolling.js
@@ -1,3 +1,35 @@
+const currentPath = window.location.pathname;
+
+if (currentPath === "/portfolio/index.html" || currentPath === "/portfolio/Projects/index.html"||
+  currentPath === "/portfolio/Game-jams/index.html") {
+  const w = window.innerWidth;
+  window.onload = function() {
+    w.innerHTML = window.innerWidth;
+    sizeChange()
+  }
+  window.onresize = function() {
+    sizeChange()
+  }
+
+  window.addEventListener('resize', sizeChange)
+  const portfolio = document.getElementById("Portfolio");
+
+  function sizeChange() {
+
+    if (portfolio) {
+      if (window.innerWidth < 100) {
+        portfolio.style.gridTemplateColumns = "repeat(1, minmax(400px, 1fr))";
+      }
+      else if (window.innerWidth >= 100 && window.innerWidth < 1841) {
+        portfolio.style.gridTemplateColumns = "repeat(2, minmax(400px, 1fr))";
+      }
+      else if (window.innerWidth >= 1841) {
+        portfolio.style.gridTemplateColumns = "repeat(3, minmax(400px, 1fr))";
+      }
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(".fade-in");
 
